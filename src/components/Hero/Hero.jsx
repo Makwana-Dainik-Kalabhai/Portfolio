328c7eec43f38b463340ec8907f396135d17f639
Makwana Dainik  Kalabhai <dainikmakwana31@gmail.com>
2025-05-17 11:20:01 +0530

    updated

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,25 +8,27 @@ const Hero = () => {
             <div className="hero-content">
                 <h2>Hey, I am a</h2>
                 <h3><ReactTyped strings={["Web Designer", "FronEnd Developer", "BackEnd Developer", "UI/UX Designer", "Problem Solver"]} typeSpeed={40} loop /></h3>
-                <p>Passionate Full-Stack Developer | Trandforming ideas in virtual world | Visually Stunning Web Solutions</p>
+                <p><b>Passionate Full-Stack Developer | Transforming ideas in virtual world | Visually Stunning Web Solutions.</b><br /> Aspiring Web Developer with foundational knowledge in HTML, CSS, jQuery, Bootstrap, and React. Seeking opportunities to leverage front-end development skills in creating dynamic, responsive
+                    websites in a collaborative team environment.
+                </p>
             </div>
 
             <div className="hero-img">
                 <div>
-                    <img src={(process.env.PUBLIC_URL)+"/assets/hero.jpg"} alt="" />
+                    <img src={(process.env.PUBLIC_URL) + "/assets/hero.jpg"} alt="" />
                 </div>
                 <div>
                     <div className="tech-icon">
-                        <img src={(process.env.PUBLIC_URL)+"/assets/techImg/react.svg"} alt="" />
+                        <img src={(process.env.PUBLIC_URL) + "/assets/techImg/react.svg"} alt="" />
                     </div>
                     <div className="tech-icon">
-                        <img src={(process.env.PUBLIC_URL)+"/assets/techImg/html.svg"} alt="" />
+                        <img src={(process.env.PUBLIC_URL) + "/assets/techImg/html.svg"} alt="" />
                     </div>
                     <div className="tech-icon">
-                        <img src={(process.env.PUBLIC_URL)+"/assets/techImg/css.svg"} alt="" />
+                        <img src={(process.env.PUBLIC_URL) + "/assets/techImg/css.svg"} alt="" />
                     </div>
                     <div className="tech-icon">
-                        <img src={(process.env.PUBLIC_URL)+"/assets/techImg/javascript.svg"} alt="" />
+                        <img src={(process.env.PUBLIC_URL) + "/assets/techImg/javascript.svg"} alt="" />
                     </div>
                 </div>
             </div>
